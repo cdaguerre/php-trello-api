fea57ecdee580b31bd6f83bc82762e9605bca257
rtvisser <reinder@clockworkfactory.com>
2021-05-25 12:29:13 +0200

    Update PowerUps.php
    Add function to retrieve all power-ups from a Trello board and to enable a power-up on the board.

--- lib/Trello/Api/Board/PowerUps.php
+++ lib/Trello/Api/Board/PowerUps.php
@@ -18,5 +18,33 @@ class PowerUps extends AbstractApi
      * Base path of board power ups api
      * @var string
      */
-    protected $path = 'boards/#id#/powerUps';
+    privated $path = 'boards/#id#/boardPlugins';
+   
+    
+    /**
+     * Get all power ups that are enabled on the board
+     * @link https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-boards-id-plugins-get
+     *
+     * @param string $id  the board's id
+     *
+     * @return array
+     */
+    public function all($boardId, array $params = array())
+    {
+        return $this->get($this->getPath($boardId), $params);
+    }
+    
+    /**
+     * Enable a powerup on the board
+     * @link https://developer.atlassian.com/cloud/trello/rest/api-group-boards/#api-boards-id-boardplugins-post
+     *
+     * @param string $boardId   the board's id
+     * @param string $powerUpId the id of the powerUp that should be enabled
+     *
+     * @return array
+     */
+    public function enable($boardId, $powerUpId)
+    {
+        return $this->put($this->getPath($boardId), array('idPlugin' => $powerUpId));
+    }
 }
